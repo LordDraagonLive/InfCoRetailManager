9cfde3ed2a792a9d5a22a0c364f81c9a3b6968db
Buddhi Adhikari <buddhiadikari@live.com>
2020-09-16 13:35:25 +0530

    Added new library to handle SQL Data Access
    Added new SP to get the logged in User. Added new SQL Data Access Library and a user model.

---
+++ InfiniumDataManager.Library/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniumDataManager.Library.Models
+{
+    public class UserModel
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
