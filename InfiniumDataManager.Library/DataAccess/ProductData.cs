0cc2929364ce20cff6264af4a4fd2a86be333c69
Buddhi Adhikari <buddhiadikari@live.com>
2020-09-27 17:00:45 +0530

    Added New API call to get all the products
    Added a new SP to retrieve all the products. Then added a productModel and a data access class to execute the SP. Finally, called the ProductData class in the newly added ProductController.

---
+++ InfiniumDataManager.Library/DataAccess/ProductData.cs
@@ -0,0 +1,22 @@
+﻿using InfiniumDataManager.Library.Internal.DataAccess;
+using InfiniumDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniumDataManager.Library.DataAccess
+{
+    public class ProductData
+    {
+        public List<ProductModel> GetProducts()
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var output = sql.LoadData<ProductModel, dynamic>("[dbo].[spProduct_GetAll]", new { }, "InfCoData");
+
+            return output;
+        }
+    }
+}
---
+++ InfiniumDataManager/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using InfiniumDataManager.Library.DataAccess;
+using InfiniumDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace InfiniumDataManager.Controllers
+{
+    [Authorize]
+    public class ProductController : ApiController
+    {
+        public List<ProductModel> Get()
+        {
+            ProductData data = new ProductData();
+
+            return data.GetProducts();
+        }
+    }
+}
