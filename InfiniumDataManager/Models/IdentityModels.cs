4ea4f9fb828b35ca4ecdee8d20fc758e861e7e93
Buddhi Adhikari <buddhiadikari@live.com>
2020-10-10 13:37:18 +0530

    Update EF Database Name & Location
    Updates the EF database that holds login user details to be InfCoEFData and set the location to SQL Server.

--- InfiniumDataManager/Models/IdentityModels.cs
+++ InfiniumDataManager/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace InfiniumDataManager.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("InfCoEFData", throwIfV1Schema: false)
         {
         }
         
